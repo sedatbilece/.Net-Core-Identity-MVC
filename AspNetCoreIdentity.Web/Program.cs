feb55bad7574bc58b0198f2d9e44ea866c837c22
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2023-02-07 15:54:29 +0300

    Build IdentityDbContext structure

--- AspNetCoreIdentity.Web/Program.cs
+++ AspNetCoreIdentity.Web/Program.cs
@@ -1,7 +1,18 @@
+using AspNetCoreIdentity.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+});
+
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<AppDbContext>();
+
 
 var app = builder.Build();
 
