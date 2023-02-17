4ee0f360d96a13888b24e9e84297c4072515b137
sedatbilece-mobitek <sedat.bilece@mobitek.com>
2023-02-17 11:54:03 +0300

    mob

--- AspNetCoreIdentity.Web/Program.cs
+++ AspNetCoreIdentity.Web/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
 });
 
-//sedat bilece test2 main
+//sedat bilece test2 mob
 
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
 
