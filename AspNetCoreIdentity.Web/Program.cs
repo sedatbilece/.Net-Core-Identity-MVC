24b57e36fd75bf47e9100ea303eb00134e343888
Sedat Bilece <seda1tbilece@gmail.com>
2023-02-17 11:53:37 +0300

    main

--- AspNetCoreIdentity.Web/Program.cs
+++ AspNetCoreIdentity.Web/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
 });
 
-//sedat bilece test2 m
+//sedat bilece test2 main
 
 builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
 
