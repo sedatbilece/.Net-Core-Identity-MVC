7f1e2a77be4507389b183bd2f8db381b4e445861
sedatbilece-mobitek <sedat.bilece@mobitek.com>
2023-02-20 15:08:31 +0300

    RoleListViewModel

---
+++ AspNetCoreIdentity.Web/Areas/Admin/Models/RoleListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreIdentity.Web.Areas.Admin.Models
+{
+    public class RoleListViewModel
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
